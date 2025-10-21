c1b1a5852c916801a32a77d9dd97a23f47a18793
ATM Abdullah Al Mamun <atmabdullaham@gmail.com>
2025-10-21 12:40:25 +0600

    Implement Marquee & Design Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
+import { format } from "date-fns";
 import logo from "../assets/logo.png";
 const Header = () => {
   return (
-    <div className="flex justify-center flex-col items-center gap-3">
-      <img src={logo} alt="" />
-      <p>Journalism Without Fear or Favour </p>
+    <div className="flex justify-center flex-col items-center gap-3 py-2">
+      <img className="w-[400px]" src={logo} alt="" />
+      <p className="text-accent">Journalism Without Fear or Favour </p>
+      <p>{format(new Date(), "EEEE, MMMM MM, yyyy")}</p>
     </div>
   );
 };
